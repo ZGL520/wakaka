c3a99e61e789eb2c26de2d711b8775ff47b15b63
unknown <wb-zgl592226@alibaba-inc.com>
2019-08-28 14:49:04 +0800

    fix

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 80;
 const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello zgl');
-res.error(console.log('err'));
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello zgl');
+  // res.error(console.log('err'));
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
